cb33457a4e92d137eb2019f6294a7adab6f1a8d1
Ratul Kulkarni <ratulkulkarni10@gmail.com>
2025-08-20 14:09:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    return res.json({
+        message : "Dockerized Node js"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started on PORT : ${PORT}`);
+})
